ecb4823510f2a9af661f45cfd7ee18f9fb624ad2
ByUsi-link <14266085+shibeiane@user.noreply.gitee.com>
2025-02-20 14:47:10 +0800

    first commit

--- git.sh
+++ git.sh
@@ -1,3 +1,5 @@
+git config --global user.name '北啊呢' 
+git config --global user.email '14266085+shibeiane@user.noreply.gitee.com'
 git add .
 git commit -m "first commit"
 git remote add origin https://gitee.com/byusi/docs.git
